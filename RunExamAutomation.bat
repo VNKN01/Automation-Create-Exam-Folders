71110e6a7542f93a4d63ce870f34a51d76fd46d8
Syed Hammad Ahmed <44769452+hammadattari@users.noreply.github.com>
2026-01-15 13:49:06 +0500

    Update script to call Create-ExamFolders-V2.ps1

--- RunExamAutomation.bat
+++ RunExamAutomation.bat
@@ -1,9 +1,10 @@
 @echo off
 setlocal
 set SCRIPT_DIR=%~dp0
-PowerShell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%Create-ExamFolders.ps1"
+PowerShell -NoProfile -ExecutionPolicy Bypass -File "%SCRIPT_DIR%Create-ExamFolders-V2.ps1"
 
 
 echo.
 echo Script finished.
 pause
+
